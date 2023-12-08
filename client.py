dd20f1431cbaf8753e429010c4d16ce09910bda5
Jorge Costa <48180944+jotac431@users.noreply.github.com>
2023-12-08 21:32:19 +0000

    changing filenames

--- client.py
+++ client.py
@@ -1,110 +1,197 @@
 import zmq
 import uuid
 import json
-from collections import defaultdict
+import time
+import threading
 
 context = zmq.Context()
 socket = context.socket(zmq.REQ)
 socket.connect("tcp://localhost:5556")  # Connect to the server
 
-class LWWRegisterCRDT:
+# Local data storage
+shopping_lists = []
+user_id = str(uuid.uuid4())
+
+class LWWRegister:
+    def __init__(self, quantity=0, item_name='', time=0, client_id=''):
+        self.state = {'item_name': item_name, 'quantity': quantity, 'time': time, 'client_id': client_id}
+    
+    
+    def merge(self, remote):
+        if self.state['time'] < remote['time']:
+            self.state = remote
+        if self.state['time'] == remote['time']:
+            if self.state['client_id'] < remote['client_id']:
+                self.state = remote
+        
+class LWWMap:
     def __init__(self):
-        self.value = None
-        self.state = {'time': 0, 'client_id': 0, 'data': None}
-
-    def merge(self, local, remote):
-        if local['time'] > remote['time']:
-            return local
-        elif local['time'] == remote['time']:
-            if local['client_id'] > remote['client_id']:
-                return local
-        return remote
-
-class LWWMapCRDT:
-    def __init__(self):
-        self.value = {}
-        self.state = {}
-
-    def merge(self, local, remote):
-        for key in remote.keys():
-            if key in local:
-                local[key] = local[key].merge(local[key].state, remote[key].state)
+        self.list = []
+    def merge(self, remote):
+        for k in remote.list:
+            if self.list[k]:
+                self.list[k].merge(self.list[k], remote.list[k])
             else:
-                local[key] = remote[key]
+                self.list[k] = remote.list[k]
 
 class ShoppingList:
-    def __init__(self):
+    def __init__(self, name):
         self.id = str(uuid.uuid4())
-        self.list = LWWMapCRDT()
-
-# JSON file paths
-SHOPPING_LISTS_FILE = "shopping_lists.json"
-
-# Custom JSON Encoder to handle serialization of LWWRegisterCRDT objects
-class CRDTJSONEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, LWWRegisterCRDT):
-            return obj.state
-        return json.JSONEncoder.default(self, obj)
-
-# Load existing data from JSON files
-try:
-    with open(SHOPPING_LISTS_FILE, "r") as file:
-        shopping_lists_data = json.load(file)
-except FileNotFoundError:
-    shopping_lists_data = {}
-
-# Local storage using CRDTs
-shopping_lists = defaultdict(ShoppingList)
-
-def save_shopping_lists():
-    with open(SHOPPING_LISTS_FILE, "w") as file:
-        json.dump(shopping_lists_data, file, cls=CRDTJSONEncoder)
-
+        self.name = name
+        self.list = LWWMap()
+        
 def create_shopping_list(list_name):
-    shopping_list = ShoppingList()
-    shopping_list.list.value = {}
-    list_id = str(uuid.uuid4())
-    shopping_lists_data[list_id] = {
-        'list_name': list_name,
-        'list': {}
-    }
-    shopping_lists[list_id] = shopping_list
-    save_shopping_lists()
-    return list_id
+    try:
+        socket.send_json({"action": "create", "list_name": list_name})
+        poller = zmq.Poller()
+        poller.register(socket, zmq.POLLIN)
+
+        if poller.poll(timeout=2000):  # Waiting for 2 seconds for a response
+            response = socket.recv_json()
+            new_shopping_list = ShoppingList(list_name)
+            new_shopping_list.id = response["list_id"]
+            shopping_lists.append(new_shopping_list)
+            return response.get("list_id", None)
+        else:
+            print("Server is unreachable. Creating shopping list locally.")
+            new_shopping_list = ShoppingList(list_name)
+            shopping_lists.append(new_shopping_list)
+            return new_shopping_list.id  # Return the locally created ID
+    except zmq.error.ZMQError as e:
+        # Server is unreachable, simulate creating the shopping list locally
+        print("Server is unreachable. Creating shopping list locally.")
+        new_shopping_list = ShoppingList(list_name)
+        shopping_lists.append(new_shopping_list)
+        return new_shopping_list.id  # Return the locally created ID
 
 def get_list_contents(list_id):
-    return shopping_lists_data.get(list_id, {}).get('list', {})
+    # Check local shopping_lists first
+    for shopping_list in shopping_lists:
+        if shopping_list.id == list_id:
+            return shopping_list
 
+    try:
+        print("List does not exist locally. Checking server storage...")
+        socket.send_json({"action": "get_list_contents", "list_id": list_id})
+
+        poller = zmq.Poller()
+        poller.register(socket, zmq.POLLIN)
+
+        if poller.poll(timeout=2000):  # Waiting for 2 seconds for a response
+            response = socket.recv_json()
+
+            if response and response.get("status") == "success":
+                print("List " + response.get("name") + " found.")
+                # Assuming the received list_contents is a list in the response
+                received_contents = response.get("list_contents")
+                # Populate the structure on the client side
+                new_list = ShoppingList(response.get("name")) 
+                shopping_lists.append(new_list)
+                for item in received_contents:
+                    new_item = LWWRegister()  
+                    new_item.value = item["value"]
+                    new_item.state = item["state"]
+                    new_list.append(new_item)
+                return new_list
+        else:
+            print("Server is unreachable. Unable to get list contents.")
+            return None  # Return None if server is unreachable
+    except zmq.error.ZMQError as e:
+        # Handle connection-related errors
+        print("Error: ", e)
+        return None  # Return None if an error occurs
+
+def print_list_contents(contents):
+    print(f"\n\n\nList Name: {contents.name}")
+    for lww_register in contents.list.list:
+        print(f"Item Name: {lww_register.state['item_name']}, Quantity: {lww_register.state['quantity']}, Time: {lww_register.state['time']}, Client ID: {lww_register.state['client_id']}")
+
+        
 def add_item(list_id, item_name):
-    list_obj = shopping_lists[list_id].list
-    if item_name in list_obj.value:
-        list_obj.value[item_name].state['time'] += 1
-    else:
-        lww_register = LWWRegisterCRDT()
-        lww_register.state['time'] += 1
-        list_obj.value[item_name] = lww_register
-    shopping_lists_data[list_id]['list'] = list_obj.value
-    save_shopping_lists()
+    for shopping_list in shopping_lists:
+        if shopping_list.id == list_id:
+            # Check if the item already exists in the list
+            for item in shopping_list.list.list:
+                if item.state['item_name'] == item_name:
+                    # Increment quantity if item exists
+                    item.state['quantity'] += 1
+                    print("Item exists. Incremented quantity.")
+                    return True
+            
+            # If item doesn't exist, create a new LWWRegister for the item and add it to the list
+            new_item = LWWRegister(quantity=1, item_name=item_name, time=int(time.time()), client_id=user_id)
+            print("Item does not exist. Adding item.")
+            shopping_list.list.list.append(new_item)
+            return True
+    print("List ID not found.")
+    return False
+
 
 def delete_item(list_id, item_name):
-    list_obj = shopping_lists[list_id].list
-    if item_name in list_obj.value:
-        del list_obj.value[item_name]
-    shopping_lists_data[list_id]['list'] = list_obj.value
-    save_shopping_lists()   
-    
-def print_list_contents(contents):
-    print("Shopping List Contents:")
-    if not contents:
-        print("The list is empty.")
-    else:
-        for index, (item, lww_register) in enumerate(contents.items(), start=1):
-            print(f"{index}. {item} - Timestamp: {lww_register.state['time']}")
+    for shopping_list in shopping_lists:
+        if shopping_list.id == list_id:
+            for item in shopping_list.list.list:
+                if item.state['item_name'] == item_name:
+                    if item.state['quantity'] > 0:
+                        # Decrement quantity if more than 1 item exists
+                        item.state['quantity'] -= 1
+                        return True
+                    # Return True even if the quantity is 0
+                    return True
+    return False  # Return False if list_id is not found or item not found in the list
+
+
+
+# Function to periodically check server connectivity and synchronize data
+def synchronize_with_server():
+    while True:
+        try:
+            # Check server connectivity by attempting to connect
+            context_check = zmq.Context()
+            socket_check = context_check.socket(zmq.REQ)
+            socket_check.connect("tcp://localhost:5556")
+            socket_check.send_string("PING")  # Send a ping message to check connectivity
+            response = socket_check.recv_string()
+
+            if response == "PONG":  # Server responded, indicating connectivity
+                # Perform data synchronization with the server
+                # Implement logic to merge local data with server data here
+                
+                # For example:
+                # Iterate through local shopping_lists and send changes to the server
+                print("Connected!!!")
+                for local_list in shopping_lists:
+                    # Send updates to the server for each shopping list
+                    # Modify this part based on your merging logic
+                    print("Merging " + local_list.name)
+                    '''socket_check.send_json({
+                        "action": "merge_with_server",
+                        "list_id": local_list.id,
+                        "list_contents": local_list.list  # Sending local list contents to merge
+                    })
+                    _ = socket_check.recv_json()  # Receive acknowledgment from the server'''
+                    
+                # After synchronization, you can break the loop or add a delay before checking again
+                # break  # Break the loop if synchronization is done
+        except zmq.error.ZMQError as e:
+            # Handle connection errors or any other exceptions here
+            print("Connection error:", e)
+        
+        # Add a delay before the next check
+        time.sleep(10)  # Check every 10 seconds
+
+# Start the synchronization thread
+sync_thread = threading.Thread(target=synchronize_with_server)
+sync_thread.daemon = True  # Set the thread as daemon so it exits when the main thread exits
+sync_thread.start()
+
+
+
 
 
 # User Interaction
 while True:
+    print("\n\nUser ID: " + user_id)
     print("Options:")
     print("1. Create a new shopping list")
     print("2. Enter an existing shopping list")
@@ -117,71 +204,45 @@ while True:
     elif choice == "2":
         list_id = input("Enter the ID of the existing shopping list: ")
         contents = get_list_contents(list_id)
-        print_list_contents(contents)
+        if contents is not None:
+            print_list_contents(contents)
 
-        while True:
-            print("\nOptions for the shopping list:")
-            print("1. Add an item")
-            print("2. Delete an item")
-            print("3. Back to main menu")
-            list_choice = input("Enter your choice (1/2/3): ")
+            # Check if contents exist before entering the nested menu
+            while True:
+                print("\nOptions for the shopping list:")
+                print("1. Add an item")
+                print("2. Delete an item")
+                print("3. Back to main menu")
+                list_choice = input("Enter your choice (1/2/3): ")
 
-            if list_choice == "1":
-                item_name = input("Enter the name of the item to add: ")
-                add_item(list_id, item_name)
-                contents = get_list_contents(list_id)  # Update contents after adding item
-                print_list_contents(contents)
-            elif list_choice == "2":
-                contents = get_list_contents(list_id)
-                print_list_contents(contents)
-                item_name = input("Enter the name of the item to delete: ")
-                delete_item(list_id, item_name)
-                contents = get_list_contents(list_id)  # Update contents after deletion
-                print_list_contents(contents)
-            elif list_choice == "3":
-                break
+                if list_choice == "1":
+                    item_name = input("Enter the name of the item to add: ")
+                    add_item(list_id, item_name)
+                    contents = get_list_contents(list_id)  # Update contents after adding item
+                    print_list_contents(contents)
+                elif list_choice == "2":
+                    contents = get_list_contents(list_id)
+                    print_list_contents(contents)
+                    if contents:
+                        item_name = input("Enter the name of the item to delete: ")
+                        delete_item(list_id, item_name)
+                        contents = get_list_contents(list_id)  # Update contents after deletion
+                        print_list_contents(contents)
+                    else:
+                        print("The list is empty.")
+                elif list_choice == "3":
+                    break
+        else:
+            print("List ID does not exist.")
     else:
         print("Invalid choice. Please enter 1 or 2.")
-
-"""
-CRDT<T>{
-    Value
-    State
-    Merge
-}
-
-LWWRegister CRDT<T>{
-    Value
-    State:{time, client_id, T}
-    merge(local, remote)
-        if local.time > remote.time
-            local
-        if local.time == remote.time
-            if local.client_id > remote.client_id
-                local
-        else
-            remote
         
-}
-
-LWWMap <T>{
-    Value
-    State: %{"onions" => LWWRegister<Integer>}
-    merge(local, remote){
-        for k in keys remote{
-            if local[k]
-                merge(local[k], remote[k])
-            else
-                local[k] = remote[k]
-        }
-    }
-}
-
-struct ShoppingList{
-    id: UUID client
-    list: LWWMap<T>
-}
-
-
-
-"""
+        
+        
+        
+        
+        
+        
+        
+        
+        
--- server.py
+++ server.py
@@ -1,49 +1,133 @@
 import zmq
 import uuid
 import json
-from collections import defaultdict
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:5556")  # Bind to all network interfaces on port 5556
 
-# Simulated in-memory data storage for shopping lists
-shopping_lists = defaultdict(dict)
+# Data storage
+shopping_lists = []
 
-def print_shopping_lists():
-    print("Current Shopping Lists:")
-    for list_id, data in shopping_lists.items():
-        print(f"List ID: {list_id}, Name: {data['list_name']}")
-        print("Items:")
-        for item in data['items']:
-            print(f"- {item['name']}: Quantity - {item['quantity']}")
-        print("")
+class LWWRegister:
+    def __init__(self, quantity=0, item_name='', time=0, client_id=''):
+        self.state = {'item_name': item_name, 'quantity': quantity, 'time': time, 'client_id': client_id}
+    
+    
+    def merge(self, remote):
+        if self.state['time'] < remote['time']:
+            self.state = remote
+        if self.state['time'] == remote['time']:
+            if self.state['client_id'] < remote['client_id']:
+                self.state = remote
+        
+class LWWMap:
+    def __init__(self):
+        self.list = []
+    def merge(self, remote):
+        for k in remote.list:
+            if self.list[k]:
+                self.list[k].merge(self.list[k], remote.list[k])
+            else:
+                self.list[k] = remote.list[k]
+        
+class ShoppingList:
+    def __init__(self, name):
+        self.id = str(uuid.uuid4())
+        self.name = name
+        self.list = LWWMap()
+        
+def handle_create(message):
+    list_name = message.get("list_name")
+    print("Creating store named " + list_name)
+    response = {}
+    
+    new_list = ShoppingList(list_name)
+    shopping_lists.append(new_list)
+    response["success"] = True
+    response["message"] = "Shopping list created successfully."
+    response["list_id"] = new_list.id
+        
+    return response
 
-def add_or_increment_item(list_id, item_name):
-    for item in shopping_lists[list_id]["items"]:
-        if item["name"] == item_name:
-            item["quantity"] += 1
-            return True  # Found and incremented the quantity for the existing item
-    # If item not found, add a new item to the list
-    shopping_lists[list_id]["items"].append({"name": item_name, "quantity": 1})
-    return True  # Added a new item
+def handle_get_list_contents(message):
+    list_id = message.get("list_id")
+    response = {}
+    print("Retreiving list with id " + list_id)
+    
+    found_list = None
+    for shopping_list in shopping_lists:
+        if shopping_list.id == list_id:
+            found_list = shopping_list
+            print("Found list with name " + found_list.name)
+            break
+    
+    if found_list:
+        response["status"] = "success"
+        response["name"] = found_list.name
+        response["list_contents"] = [{
+            "value": item.value,
+            "state": item.state
+        } for item in found_list.list.list]
+        print("Sending list " + found_list.name)
+    else:
+        response["status"] = "error"
+        response["message"] = "List not found"
 
-def delete_or_decrement_item(list_id, item_index):
-    if 0 <= item_index < len(shopping_lists[list_id]["items"]):
-        item = shopping_lists[list_id]["items"][item_index]
-        if item["quantity"] > 1:
-            item["quantity"] -= 1
-        else:
-            del shopping_lists[list_id]["items"][item_index]
-        return True  # Deleted or decremented the quantity for the existing item
-    return False  # Invalid index
+    return response
+
+
 
 while True:
-    message = socket.recv_json()  # Receive JSON message from the client
-    action = message.get("action")
+    message = socket.recv_string()  # Receive string message from the client
+    print(message)
     response = {}
+    
+    try:
+        if message == "PING":  # Handle the "PING" message separately
+            # Respond to the client to confirm server connectivity
+            socket.send_string("PONG")
+            continue  # Skip JSON deserialization for "PING" messages
+        
+        try:
+            received_json = json.loads(message)  # Attempt JSON deserialization
+            if isinstance(received_json, dict):
+                action = received_json.get("action")
+        
+                if action == "create":
+                    response = handle_create(received_json)
+                elif action == "add":
+                    response = handle_add(received_json) # TODO
+                elif action == "get_list_contents":
+                    response = handle_get_list_contents(received_json)
+                elif action == "delete":
+                    response = handle_delete(received_json) # TODO
+                elif action == "update_quantity":
+                    response = handle_update_quantity(received_json) # TODO
+                # Add more handlers for other actions
+                
+                socket.send_json(response)
+            else:
+                logger.error("Received message is not a dictionary")
+                response["status"] = "error"
+                response["message"] = "Invalid message format"
+                socket.send_json(response)
+        except json.JSONDecodeError:
+            logger.error("Received invalid JSON message")
+            response["status"] = "error"
+            response["message"] = "Invalid JSON format"
+            socket.send_json(response)
+    except Exception as e:
+        logger.error(f"An error occurred: {e}")
+        response["status"] = "error"
+        response["message"] = "Internal server error"
+        socket.send_json(response)
 
-    if action == "create":
+    """if action == "create":
         new_list_id = str(uuid.uuid4())
         shopping_lists[new_list_id] = {"list_name": message.get("list_name"), "items": []}
         response["status"] = "success"
@@ -81,4 +165,4 @@ while True:
             response["status"] = "error"
             response["message"] = "List not found"
 
-    socket.send_json(response)  # Send JSON response back to the client
+    socket.send_json(response)  # Send JSON response back to the client"""
